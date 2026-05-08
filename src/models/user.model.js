a2801a3379725ade4c29a3a4bc31d33b6c185c8e
lucas-prat-dev <pratlucas3@gmail.com>
2026-05-08 06:45:39 -0300

    algunos ajustes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,9 +4,27 @@ const userSchema = new mongoose.Schema(
   {
     first_name: { type: String, required: true },
     last_name: { type: String, required: true },
-    email: { type: String, required: true, unique: true, lowercase: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      //verificar formato de correo con expresión regular
+      match: [
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+        "Por favor ingrese un correo electrónico válido",
+      ],
+    },
     age: { type: Number, required: true },
-    password: { type: String, required: true },
+    password: {
+      type: String,
+      required: true,
+      //verificar que la contraseña tenga al menos 8 caracteres, una mayúscula, una minúscula y un número
+      match: [
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d@$!%*?&]{8,}$/,
+        "La contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula y un número",
+      ],
+    },
     cart: { type: mongoose.Schema.Types.ObjectId, ref: "Cart" },
     role: { type: String, default: "user", enum: ["user", "admin"] },
   },
